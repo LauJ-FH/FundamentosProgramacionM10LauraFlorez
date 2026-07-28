7c8cd795119ee661834a600abab053c1e385eacc
LauJ-FH <lauraj.florez@upb.edu.co>
2026-07-28 11:35:45 -0500

    Laura FH

---
+++ Variables.Constantes/Variables.Constantes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Variables.Constantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            string nombre="Laura FH";
+            Console.WriteLine("Bienvenido"+ nombre +" al curso de fundamentos de programacion");
+        }
+    }
+}
