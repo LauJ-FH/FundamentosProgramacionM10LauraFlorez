f8cc6db4d90578b4bea540ac37bb9a1f4036113e
LauJ-FH <lauraj.florez@upb.edu.co>
2026-08-25 11:28:24 -0500

    8Contadores y acumuladores

---
+++ Contadores y acumuladores LF/Contadores y acumuladores LF/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Contadores_y_acumuladores_LF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int contador = 1;
+            int acumulador = 0;
+
+            while (contador <=5)
+            {
+                acumulador = acumulador + contador;
+                contador++;
+            }
+
+            Console.WriteLine($"la suma de los cinco primeros numeros es: {acumulador}");*/
+            int número;
+            int contador = 1;
+            int acumulador = 1;
+
+            Console.WriteLine("Ingrese el nuemro:");
+            número = int .Parse(Console.ReadLine());
+
+            while (contador <= número)
+            {
+                acumulador = acumulador + contador;
+                contador++;
+            }
+
+            Console.WriteLine($"El factorial del número es: {acumulador}");
+        }
+    }
+}
