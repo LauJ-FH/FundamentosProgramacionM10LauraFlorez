9aa7794834ea536905a466d75be2f0a5ff502846
LauJ-FH <lauraj.florez@upb.edu.co>
2026-09-03 11:28:33 -0500

    Add files via upload

---
+++ CicloForLH/CicloForLH/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CicloForLH
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumulador = 0;
+
+            for (int contador=1; contador<=5;contador++)
+            {
+                acumulador += contador;
+            }
+
+            Console.WriteLine($"La suma de l9os cinco primeros numeros enteros es: {acumulador}");
+        }
+    }
+}
