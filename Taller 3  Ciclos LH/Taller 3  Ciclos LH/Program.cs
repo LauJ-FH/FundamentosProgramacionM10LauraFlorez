c67fb99152971c623a8f0dc14da454193b6c1ad9
LauJ-FH <lauraj.florez@upb.edu.co>
2026-09-08 11:27:23 -0500

    Add files via upload

---
+++ Taller 3  Ciclos LH/Taller 3  Ciclos LH/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller_3__Ciclos_LH
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Algoritmo que permita calcular el promedio de calificaciones, el algoritmo le permitirá al usuario, introducir tantas calificaciones como así desee en el momento en que seleccione que no desea continuar capturando calificaciones, el algoritmo debe presentar el promedio de calificaciones capturadas previamente.
+
+            //double Calificacion, suma = 0, promedio;
+            //int cantidad = 0;
+            //char continuar;
+
+            //do
+            //{
+            //    Console.WriteLine("Ingrese una calificacion:");
+
+            //    Calificacion = double.Parse(Console.ReadLine());
+
+            //    suma = suma + Calificacion;
+            //    cantidad = cantidad + 1;
+
+            //    Console.Write("¿Desea ingresar otra calificacion? (S/N):");
+            //    continuar = char.Parse(Console.ReadLine());
+            //} while (continuar == 'S' || continuar == 's');
+
+            //promedio = suma / cantidad;
+
+            //Console.WriteLine($"El promedio de las calificaciones es: {promedio}");
+
+            //Se requiere un algoritmo para mostrar por pantalla los divisores de un
+            //Tener en cuenta que dados dos números enteros a y b, se dice que b es
+            //divisor de a si se cumple que al efectuar una división entera a/ b el
+            //residuo es 0, en C# utilizar el operador Mod para obtener el residuo de 
+            //una división de dos números.
+
+            int numero;
+
+            Console.Write("Ingrese un numero entero:");
+            numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Los divisores de ")
+        }
+    }
+}
